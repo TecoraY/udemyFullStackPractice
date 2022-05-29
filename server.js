ca832b5d705468a70f181f12f25b11db137b2bfa
TecoraY <102709566+TecoraY@users.noreply.github.com>
2022-05-28 21:57:13 -0400

    routes to new pages created and user schema created

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express= require('express');
-
+const connectDB=require('./config/db');
 const app= express();
-
+//connect database
+connectDB();
 app.get('/', (request, response)=> response.send('API Running'));
+
+//define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
 //process.env.port allows it to be found when running with heroku
 //|| 5000, says that locally the page should be ran from port 5000
 const PORT=process.env.PORT || 5000;
