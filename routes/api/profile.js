b2f903c032fc68365a29ba663dc6088f61bc4b0e
TecoraY <102709566+TecoraY@users.noreply.github.com>
2022-06-01 19:16:56 -0400

    profile get request created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,24 @@
 const express =require ('express');
 const router = express.Router();
+const auth=require('../../middleware/auth');//will protect the route to getuser profile
 
-//@route  GET api/profile
-//@desc   Test route
-//@access Public
-router.get('/', (request, response) => response.send('Profile route'));
+const Profile = require('../models/Profile');
+//@route  GET api/profile/me
+//@desc   get current user profile
+//@access private
+
+//route to get the profile
+router.get('/me', auth, async (request, response) => {
+    try{
+        const profile=await Profile.findOne({user: request.user.id}).populate('user', ['name', 'avatar']);
+        if(!profile){
+            return response.status(400).json({msg:'There is no profile for this user'});
+        }
+        response.json(profile);
+    }catch(err){
+        console.error(err.message);
+        response.status(500).send('Server Error');
+    }
+});
 
 module.exports= router;
